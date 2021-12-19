60a3981c32e887e8beea8a7d08e50e3bea829acf
Lashen Kariyawasam <56374597+LashenKariyawasam@users.noreply.github.com>
2021-12-20 00:23:01 +0530

    prevent itemNo duplication

--- main.c
+++ main.c
@@ -8,7 +8,11 @@ struct items {
 };
 
 void buy();
+
 void itemList();
+
+int itemNo();
+
 void addItems();
 
 int main() {
@@ -32,7 +36,7 @@ int main() {
 }
 
 
-void itemList(){
+void itemList() {
     FILE *itemsfp;
     struct items item;
     itemsfp = fopen("itemdata.txt", "r");
@@ -43,7 +47,8 @@ void itemList(){
         printf("                 itemID\t    itemName\tprice\n");
         printf("--------------------------------------------------------------------");
         while (fread(&item, sizeof(item), 1, itemsfp)) {
-            printf("\n                   %-10d%-12s%-6d                          ", item.itemID, item.itemName, item.price);
+            printf("\n                   %-10d%-12s%-6d                          ", item.itemID, item.itemName,
+                   item.price);
         }
         fclose(itemsfp);
         printf("\n--------------------------------------------------------------------\n");
@@ -56,30 +61,53 @@ void buy() {
 }
 
 
-void addItems(){
+void addItems() {
     itemList();
+    int selection = 1;
     FILE *itemfp;
-    int number = 0;
     struct items item;
-    itemfp = fopen("itemdata.txt","a");
+    itemfp = fopen("itemdata.txt", "a+");
 
-    if(itemfp==NULL){
+    if (itemfp == NULL) {
         printf("Couldn't open\n");
-    } else{
-        printf("How much item you add:");
-        scanf("%d",&number);
-        for (int i=0;i<number;++i){
-            printf("Enter item number:");
-            scanf(" %d",&item.itemID);
+    } else {
+        do {
+            item.itemID = itemNo();
+            printf("%d", item.itemID);
             printf("Enter item name:");
-            scanf(" %s",&item.itemName);
+            scanf(" %s", &item.itemName);
             printf("Enter item price:");
-            scanf(" %d",&item.price);
-            fwrite( &item,sizeof(item),1,itemfp );
-        }
-
+            scanf(" %d", &item.price);
+            fwrite(&item, sizeof(item), 1, itemfp);
+            printf("*** Add Item Successfully ***");
+            printf("\nDo you want to add another Item (1)");
+            printf("\nor exit(0)");
+            scanf("%d", &selection);
+        } while (selection);
     }
 
+
+fclose(itemfp);
+
+main();
+
+}
+
+int itemNo() {
+    int itemID, allocated;
+    printf("Enter item number:");
+    scanf(" %d", &itemID);
+    FILE *itemfp;
+    itemfp = fopen("itemdata.txt", "r");
+    struct items item;
+    while (fread(&item, sizeof(item), 1, itemfp)) {
+        if (item.itemID == itemID) {
+            allocated = 1;
+        }
+    }
     fclose(itemfp);
-    main();
+    if (allocated == 0)
+        return itemID;
+    else
+        return itemNo();
 }
