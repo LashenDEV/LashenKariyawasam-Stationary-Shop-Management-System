70f5fe980bf872c012628fa7905603a35f996455
Lashen Kariyawasam <56374597+LashenKariyawasam@users.noreply.github.com>
2021-12-19 13:23:02 +0530

    buy and addItem function

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct items {
+    int itemID;
+    char itemName[20];
+    int price;
+};
+
 void buy();
+void addItems();
 
 int main() {
     int selection;
@@ -9,14 +16,61 @@ int main() {
     printf("                             === WELCOME TO THE SHOP ===                                       \n");
     printf("----------------------------------------------------------------------------------------------------\n");
     printf("1. Buy \n");
+    printf("2. Login as Admin \n");
     printf("----------------------------------------------------------------------------------------------------\n");
     scanf("%d", &selection);
     switch (selection) {
         case 1:
             buy();
+            break;
+        case 2:
+            addItems();
+            break;
     }
 }
 
 void buy() {
-    printf("hello");
+    FILE *itemsfp;
+    struct items item;
+    itemsfp = fopen("itemdata.txt", "r");
+    if (itemsfp == NULL) {
+        printf("Couldn't open\n");
+    } else {
+        printf("\n--------------------------------------------------------------------\n");
+        printf("itemID\t\titemName\t\tprice\n");
+        printf("--------------------------------------------------------------------");
+        while (fread(&item, sizeof(item), 1, itemsfp)) {
+            printf("\n%d\t\t%s\t\t\t%d", item.itemID, item.itemName, item.price);
+        }
+        fclose(itemsfp);
+        printf("\n--------------------------------------------------------------------\n");
+    }
+}
+
+
+void addItems(){
+    FILE *itemfp;
+    int number = 0;
+    struct items item;
+    itemfp = fopen("itemdata.txt","w");
+
+    if(itemfp==NULL){
+        printf("Couldn't open\n");
+    } else{
+        printf("How much item you add:");
+        scanf("%d",&number);
+        for (int i=0;i<number;++i){
+            printf("Enter item number:");
+            scanf(" %d",&item.itemID);
+            printf("Enter item name:");
+            scanf(" %s",&item.itemName);
+            printf("Enter item price:");
+            scanf(" %d",&item.price);
+            fwrite( &item,sizeof(item),1,itemfp );
+        }
+
+    }
+
+    fclose(itemfp);
+    main();
 }
